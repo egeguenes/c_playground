039eb37e39caffe8590a15ee5f19b412b9d32383
egeguenes <senugege@gmail.com>
2025-02-09 00:26:53 +0100

    linkcount blatt 4 aufgabe

---
+++ link_count/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <errno.h>
+
+int getNumLinks(const char* filename);
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s <file/path> ...\n", argv[0]);
+        exit(1);
+    }
+
+    for(int i=1; i<argc; i++) {
+        const char* filename = argv[i];
+        int num = getNumLinks(filename);
+        if (num == -1) {
+            perror("Error");
+        } else {
+            printf("%s: %d\n", filename, num);
+        }
+    }
+    exit(0);
+}
+
+int getNumLinks(const char* filename) {
+    struct stat *st;
+    if (stat(filename, st) == -1) {
+        return -1;
+    }
+    return st->st_nlink;
+}
